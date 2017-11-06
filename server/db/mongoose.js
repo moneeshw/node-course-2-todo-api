cf773ee87f7b6fdf8166ba805be526a5b882f49f
Moneesh Walia <moneesh.walia@gmail.com>
2017-11-06 16:43:36 +0530

    Add /users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+//mongoose.set('debug', true);
+
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
